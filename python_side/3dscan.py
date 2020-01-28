d54fd84868865eedbbc139f377b514e51b0f3e0a
Mikhail Babich <babich-mv@yandex.ru>
2020-01-29 04:11:34 +1000

    Add simple circle detection

--- python_side/3dscan.py
+++ python_side/3dscan.py
@@ -33,12 +33,38 @@ imgpoints = [] # 2d points in image plane.
 
 axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
 
+
 def draw(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
     img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
     img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
     img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
     return img
+
+
+def pomoika(img):
+    mask = cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), np.array([0, 50, 50]), np.array([10, 255, 255]))
+    return cv2.bitwise_and(img, img, mask=mask)
+
+
+def detectCircles(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_blurred = cv2.blur(gray, (3, 3))
+    cv2.imshow('q', gray_blurred)
+    # TODO
+    detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10, maxRadius=100)
+    print(detected_circles)
+    return detected_circles
+
+
+def drawDetectedCircles(img, detected_circles):
+    if detected_circles is not None:
+        for circle in detected_circles:
+            x = int(circle[0][0])
+            y = int(circle[0][1])
+            r = int(circle[0][2])
+            cv2.rectangle(img, (x - r - 5, y - r - 5), (x + r + 5, y + r + 5), (255, 0, 0), 2)
+
 
 cam = cv2.VideoCapture(0)
 
@@ -105,6 +131,11 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('undistorted', dst)
+
+    circlesFrame = pomoika(frame)
+    circles = detectCircles(circlesFrame)
+    drawDetectedCircles(circlesFrame, circles)
+    cv2.imshow('RedDotWindow', circlesFrame)
 
     ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
     # If found, add object points, image points (after refining them)
