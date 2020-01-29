5eaff1113feca8689d207e945591987bd52ebabd
Nikita Dobrynin <nikita.dobrinin@mytona.com>
2020-01-29 21:18:52 +1000

    Add support for stereocam

---
+++ unity_side/3DUnityUltraScan/3dScanUnityUltraScan/Assets/Scripts/CameraPoseSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPoseSetter : MonoBehaviour
+{
+    public UDP Udp;
+    public int index;
+    public Color color;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var t = Udp.CamData[index];
+
+        transform.position = t.position;
+        transform.rotation = t.rotation;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, -transform.TransformDirection(Vector3.forward) * 50);
+    }
+}
